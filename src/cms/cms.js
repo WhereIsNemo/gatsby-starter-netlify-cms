84b5591375fe288d1f3b3fc6ad4eda28fa348b7f
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-18 15:16:13 +0600

    upgrade netlify CMS plugin for Gatsby

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,5 @@
-import CMS from 'netlify-cms';
+import CMS from "netlify-cms";
+import "netlify-cms/dist/cms.css";
 
 import CategoryPagePreview from './preview-templates/CategoryPagePreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
