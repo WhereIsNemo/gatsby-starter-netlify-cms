d4136fc7d239553b7748d2151ca9a28f4852c1a0
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-18 15:20:25 +0600

    fix Product component name

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -3,19 +3,7 @@ import PropTypes from 'prop-types'
 
 export const ProductPageTemplate = ({ title }) => {
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    {title}
   )
 }
 
@@ -27,7 +15,7 @@ const ProductPage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
-    <ProductPagePageTemplate
+    <ProductPageTemplate
       title={post.frontmatter.title}
     />
   )
