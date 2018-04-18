ec37e8f7dcc9cda959e79a50b2124dbe2bedd18c
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-18 15:51:38 +0600

    fixing and clear up of CMS config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,9 @@
+import React from "react";
 import CMS from "netlify-cms";
 import "netlify-cms/dist/cms.css";
 
 import CategoryPagePreview from './preview-templates/CategoryPagePreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 
-CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('categories', CategoryPagePreview)
-CMS.registerPreviewTemplate('categories', ProductPagePreview)
+CMS.registerPreviewTemplate('categories', CategoryPagePreview);
+CMS.registerPreviewTemplate('products', ProductPagePreview);
