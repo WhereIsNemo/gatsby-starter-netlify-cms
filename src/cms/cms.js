473c5f0352b7d877a81374b2a71cbf8edb8bb0c3
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-18 13:28:26 +0600

    connect products to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,8 @@
 import CMS from 'netlify-cms';
 
 import CategoryPagePreview from './preview-templates/CategoryPagePreview';
+import CategoryPagePreview from './preview-templates/ProductPagePreview';
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('categories', CategoryPagePreview)
+CMS.registerPreviewTemplate('categories', ProductPagePreview)
