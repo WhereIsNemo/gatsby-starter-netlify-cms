4f2a44217516f87513188e38942d5a465da603a1
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-24 14:40:22 +0600

    category page: stable base for the page

--- src/templates/category-page.js
+++ src/templates/category-page.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import CatalogAside from '../components/CatalogAside'
 
-export const CategoryPageTemplate = ({ title, description }) => {
+export const CategoryPageTemplate = ({ title, asideCategories }) => {
   return (
     <div className="container">
       <div className="container">
         <div className="row">
           <div className="col col-12 col-md-3">
-            <CatalogAside categories={this.props.data.asideCategories.edges} />
+            <CatalogAside categories={asideCategories.edges} />
           </div>
           <div className="col col-12 col-md-9">
             <h1>{title}</h1>
-            <div>{description}</div>
+            <div></div>
           </div>
         </div>
       </div>
@@ -19,25 +19,26 @@ export const CategoryPageTemplate = ({ title, description }) => {
   );
 }
 
-const CategoryPage = ({ data }) => {
+const CategoryPage = ({ data }, props) => {
+  console.log(props);
   const { markdownRemark: post } = data
+  const { asideCategories } = data;
 
-  return <CategoryPageTemplate title={post.frontmatter.title} />
+  return <CategoryPageTemplate 
+    title={post.frontmatter.title} 
+    asideCategories={asideCategories}
+  />
 }
 
 export default CategoryPage
 
 export const categoryPageQuery = graphql`
-  query CategoryPage($id: String!) {
-    ...asideCategories
-    ...categoryPage
-  }
-
-  fragment categoryPage on RootQueryType {
-    categoryPage: markdownRemark(id: { eq: $id }) {
+  query CategoryPageQuery($id: String!) {
+    markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
       }
     }
-  } 
+    ...asideCategories
+  }
 `
