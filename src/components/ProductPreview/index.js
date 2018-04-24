de4a4a1f07faf33d58fbeea903d670688ec0b341
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-24 17:12:00 +0600

    add image for product preview

--- src/components/ProductPreview/index.js
+++ src/components/ProductPreview/index.js
@@ -4,9 +4,13 @@ import Link from 'gatsby-link'
 const ProductPreview = ({node}) => {
   return (
     <div className="productPreview">
+      <div>
+        <Link to={node.fields.slug}><img src={node.frontmatter.image} /></Link>
+      </div>
       <h3>
         <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
       </h3>
+
     </div>
   )
 }
