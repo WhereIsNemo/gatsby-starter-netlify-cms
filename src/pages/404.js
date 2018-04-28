0158e851711413c47d4c27c0fcc9e82af560f985
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-28 15:22:42 +0600

    404 page localization

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFoundPage = () => (
   <div className="container">
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Страница не найдена</h1>
+    <p>Вы пытались перейти на страницу, которой нет</p>
   </div>
 );
 
