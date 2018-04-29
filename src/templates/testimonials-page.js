ed0d68d7718cbe48c14f9d8b59550900602481d5
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-29 13:31:52 +0600

    vk comments for testimonials

--- src/templates/testimonials-page.js
+++ src/templates/testimonials-page.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import $script from 'scriptjs';
 import { HTMLContent } from '../components/Content';
 
-export const TestimonialsPageTemplate = ({
-  title,
-  body,
-}) => {
-  return (
-    <div className="container">
-      <div className="testimonials-section">
-        <h1>{title}</h1>
-        <HTMLContent content={body} />
+export class TestimonialsPageTemplate extends React.Component {
+  render() {
+    const { title, body } = this.props;
+
+    return (
+      <div className="container">
+        <div className="testimonials-section">
+          <h1>{title}</h1>
+          <HTMLContent content={body} />
+        </div>
+        <div className="comments-section">
+          <div id="vk_comments"></div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  componentDidMount() {
+    $script('//vk.com/js/api/openapi.js?154', 'vkComments');
+
+    $script.ready('vkComments', () => {
+      VK.init({apiId: 6464457, onlyWidgets: true});
+      VK.Widgets.Comments("vk_comments", { limit: 10, attach: "photo" })
+    });
+  }
+}
 
 export default ({ data }) => {
   const { markdownRemark: post } = data;
