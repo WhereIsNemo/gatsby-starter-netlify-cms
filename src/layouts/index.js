4b8726a74da383dcd8e9650c6fee41330b71f80e
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-30 11:20:51 +0600

    Jivosite online-consultation integration

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,20 +6,33 @@ import Footer from '../components/Footer';
 import Services from '../components/Services';
 import './all.scss';
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet>
-      <title>220 Plus</title>
-      <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:700&amp;subset=cyrillic"
-        rel="stylesheet"
-      />
-    </Helmet>
-    <Header />
-    <div>{children()}</div>
-    <Services />
-    <Footer />
-  </div>
-);
+export default class TemplateWrapper extends React.Component {
+  render() {
+    const { children } = this.props;
 
-export default TemplateWrapper;
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>220 Plus</title>
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:700&amp;subset=cyrillic"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <Header />
+        <div>{children()}</div>
+        <Services />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount() {
+    // Jivosite integration
+    (function () {
+      var widget_id = 'E7x4zDRxnb'; var d = document; var w = window; function l() {
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/' + widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);
+      } if (d.readyState == 'complete') { l(); } else { if (w.attachEvent) { w.attachEvent('onload', l); } else { w.addEventListener('load', l, false); } }
+    })();
+  }
+}
