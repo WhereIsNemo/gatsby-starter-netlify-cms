339ac365ebaca61986d80728158e6da278055083
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-04-30 11:37:28 +0600

    add Yandex metrika

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -28,6 +28,32 @@ export default class TemplateWrapper extends React.Component {
   }
 
   componentDidMount() {
+    // Yandex metrika
+    (function (d, w, c) {
+      (w[c] = w[c] || []).push(function () {
+        try {
+          w.yaCounter48701150 = new Ya.Metrika({
+            id: 48701150,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+          });
+        } catch (e) { }
+      });
+
+      var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+      if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+      } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+
     // Jivosite integration
     (function () {
       var widget_id = 'E7x4zDRxnb'; var d = document; var w = window; function l() {
