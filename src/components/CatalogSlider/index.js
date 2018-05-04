bbabdc1b00a10a99ff10066950a8fad898376e37
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-05-04 19:27:39 +0600

    replace swiper with glide

--- src/components/CatalogSlider/index.js
+++ src/components/CatalogSlider/index.js
@@ -1,47 +1,57 @@
 import React from 'react';
-import Swiper from 'swiper';
 import Link from 'gatsby-link';
-import 'swiper/dist/css/swiper.css';
-import styles from './styles.module.scss';
+import Glide from '@glidejs/glide'
+import "@glidejs/glide/dist/css/glide.core.min.css";
+import "@glidejs/glide/dist/css/glide.theme.min.css";
 import Img from "gatsby-image";
+import styles from './styles.module.scss';
 
 export default class CatalogSlider extends React.Component {
   render() {
-    const slides = this.props.sliderCategories.map(({ node }, index) => (
-      <div key={index} className="swiper-slide">
-        <div className="catalog-slide">
-          <Img resolutions={node.childrenImageSharp[0].resolutions} />
-          <h3 className="catalog-slider__title">
-            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-          </h3>
+    const slides = [];
+    const bullets = [];
+
+    this.props.sliderCategories.forEach((sliderCategory, index) => {
+      const { node } = sliderCategory;
+
+      slides.push(
+        <div key={index} className="glide__slide">
+          <div className="catalog-slide">
+            <Img resolutions={node.childrenImageSharp[0].resolutions} />
+            <h3 className="catalog-slider__title">
+              <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+            </h3>
+          </div>
         </div>
-      </div>
-    ));
+      );
+    });
 
     return (
       <div className="section">
         <h2 className="section__title">Смотрите каталог</h2>
         <div className="catalog-slider">
-          <div className="swiper-container" ref="slider">
-            <div className="swiper-wrapper">{slides}</div>
+          <div className="glide" ref="slider">
+            <div data-glide-el="track" className="glide__track">
+              <div className="glide__slides">{slides}</div>
+            </div>
 
-            <div className="swiper-button-prev" />
-            <div className="swiper-button-next" />
+            <div className="glide__arrows" data-glide-el="controls">
+              <button className={`glide__arrow glide__arrow--prev`} data-glide-dir="<">prev</button>
+              <button className={`glide__arrow glide__arrow--next ${styles.sliderArrowNext}`} data-glide-dir=">">next</button>
+            </div>
           </div>
         </div>
       </div>
     );
   }
   componentDidMount() {
-    new Swiper(this.refs.slider, {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+    new Glide(this.refs.slider, {
+      perView: 4,
+      gap: 30,
+      focusAt: "center",
+      autoplay: 4500,
+      animationDuration: 1000,
+    }).mount();
   }
 }
 
--- src/components/SpecialOffersSlider/index.js
+++ src/components/SpecialOffersSlider/index.js
@@ -1,41 +1,50 @@
 import React from 'react';
-import Swiper from 'swiper';
-import 'swiper/dist/css/swiper.css';
 import Img from "gatsby-image";
+import Glide from '@glidejs/glide'
+import "@glidejs/glide/dist/css/glide.core.min.css";
+import "@glidejs/glide/dist/css/glide.theme.min.css";
 import styles from './styles.module.scss';
 
 export default class Slider extends React.Component {
   render() {
     const { specialOffers } = this.props;
 
-    const slides = specialOffers.map(({ node }, index) => (
-      <div key={index} className="swiper-slide">
-        <Img sizes={node.childrenImageSharp[0].sizes} />
-      </div>
-    ));
+    const slides = [];
+    const bullets = [];
+
+    specialOffers.forEach((specialOffer, index) => {
+      slides.push(
+        <div key={index} className="glide__slide">
+          <Img sizes={specialOffer.node.childrenImageSharp[0].sizes} />
+        </div>
+      );
+
+      bullets.push(
+        <button key={index} className="glide__bullet" data-glide-dir={`=${index}`}></button>
+      );
+    });
 
     return (
-      <div className="swiper-container" ref="slider">
-        <div className="swiper-wrapper">{slides}</div>
-        <div className="swiper-pagination" />
+      <div className="glide" ref="slider">
+        <div data-glide-el="track" className="glide__track">
+          <div className="glide__slides">{slides}</div>
+        </div>
 
-        <div className="swiper-button-prev" />
-        <div className="swiper-button-next" />
+        <div className="glide__arrows" data-glide-el="controls">
+          <button className={`glide__arrow glide__arrow--prev`} data-glide-dir="<">prev</button>
+          <button className={`glide__arrow glide__arrow--next ${styles.sliderArrowNext}`} data-glide-dir=">">next</button>
+        </div>
+        <div className="glide__bullets" data-glide-el="controls[nav]">
+          {bullets}
+        </div>
       </div>
     );
   }
   componentDidMount() {
-    new Swiper(this.refs.slider, {
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+    new Glide(this.refs.slider, {
+      autoplay: 7000,
+      animationDuration: 2000,
+    }).mount();
   }
 }
 
