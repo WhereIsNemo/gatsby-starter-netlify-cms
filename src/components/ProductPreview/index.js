b648ae10c2bf54a10da92116420e4b15efd4d6fe
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-05-10 12:29:53 +0600

    product preview enhancements

--- src/components/ProductPreview/index.js
+++ src/components/ProductPreview/index.js
@@ -7,15 +7,13 @@ import styles from './styles.module.scss';
 const Preview = ({ node }) => (
   <div className={styles.preview}>
     <div className={styles.imgWrapper}>
-      <Link to={node.fields.slug}>
-        <Img resolutions={node.childrenImageSharp[0].resolutions} />
-      </Link>
+      <Img resolutions={node.childrenImageSharp[0].resolutions} />
     </div>
     <h3 className={styles.title}>
       <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
     </h3>
     <div className={styles.price}>{node.frontmatter.price}₽</div>
-    <div>
+    <div className={styles.orderCallbackField}>
       <OrderCallbackButton additionalText={node.frontmatter.title} />
     </div>
   </div>
