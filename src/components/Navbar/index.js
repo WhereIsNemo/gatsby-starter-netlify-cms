8473b00dd714cdc84e0c880933d51bdb0f3c7c10
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-05-11 15:26:06 +0600

    responsive theming: mobile navigation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,51 +2,83 @@ import React from 'react';
 import Link from 'gatsby-link';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faBars from '@fortawesome/fontawesome-free-solid/faBars';
+import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 import styles from './styles.module.scss';
 
-const Navbar = () => {
-  const links = [
-    {
-      text: "Главная",
-      to: "/",
-    },
-    {
-      text: "Каталог",
-      to: "/catalog",
-    },
-    {
-      text: "Доставка и оплата",
-      to: "/delivery",
-    },
-    {
-      text: "Склад самовывоза",
-      to: "/warehouse",
-    },
-    {
-      text: "Отзывы",
-      to: "/testimonials",
-    },
-    {
-      text: "Контакты",
-      to: "/contact",
-    },
-  ];
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const menuItemsMarkup = links.map(({ text, to }, index) =>
-    <li key={index}>
-      <Link className={styles.menuLink} to={to}>
-        {text}
-      </Link>
-    </li>
-  );
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
 
-  return (
-    <nav>
-      <ul className={styles.menu}>
-        {menuItemsMarkup}
-      </ul>
-    </nav>
-  )
-};;
+  handleMenuToggle = () => {
+    this.setState({
+      isMenuOpen: !this.state.isMenuOpen,
+    })
+  }
 
-export default Navbar;
+  handleLinkClick = () => {
+    this.setState({
+      isMenuOpen: false,
+    })
+  }
+
+  render() {
+    const { isMenuOpen } = this.state;
+
+    const links = [
+      {
+        text: "Главная",
+        to: "/",
+      },
+      {
+        text: "Каталог",
+        to: "/catalog",
+      },
+      {
+        text: "Доставка и оплата",
+        to: "/delivery",
+      },
+      {
+        text: "Склад самовывоза",
+        to: "/warehouse",
+      },
+      {
+        text: "Отзывы",
+        to: "/testimonials",
+      },
+      {
+        text: "Контакты",
+        to: "/contact",
+      },
+    ];
+
+    const menuItemsMarkup = links.map(({ text, to }, index) =>
+      <li key={index}>
+        <Link className={styles.menuLink} to={to} onClick={this.handleLinkClick}>
+          {text}
+        </Link>
+      </li>
+    );
+
+    return (
+      <nav>
+        <button
+          className={styles.toggler}
+          type="button"
+          onClick={this.handleMenuToggle}
+        >
+          {isMenuOpen && <FontAwesomeIcon icon={faTimes} />}
+          {!isMenuOpen && <FontAwesomeIcon icon={faBars} />}
+        </button>
+
+        <ul className={`${ isMenuOpen && 'isVisible'} ${styles.menu}`}>
+          {menuItemsMarkup}
+        </ul>
+      </nav>
+    );
+  }
+}
